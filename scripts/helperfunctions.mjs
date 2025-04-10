6f83cb77ffefd33edd35ca468472493b40701481
m-meno <mackenzie.meno@gmail.com>
2025-04-10 16:51:36 -0600

    helper function to display data

--- scripts/helperfunctions.mjs
+++ scripts/helperfunctions.mjs
@@ -2,15 +2,27 @@
 
 (async function getData(){
     try {
-        await fetch(`https://api.thedogapi.com/v1/breeds`, {
+        let res = await axios.get(`https://api.thedogapi.com/v1/breeds`, {
         headers: {
         "x-api-key": `live_hudKAJxap4eIfy2oapeBY0rIq9CVN2fvPRXatZUGlqprvho2VPV67IHJGVM7RXh2`
         } });
+        createDisplay(res.data);
     } catch(err) {
       console.error(`Error`)
     }
 })();
 
 async function createDisplay(dataArray){
-    const main = document.getElementById(`main`)
+    const main = document.getElementById(`main`);
+    let list = document.createElement(`ul`);
+    main.appendChild(list);
+    console.log(dataArray);
+    dataArray.forEach((el)=>{
+        let listItem = document.createElement(`li`);
+        listItem.innerHTML = `Breed name: ${el.name}`
+        list.appendChild(listItem);
+    })
+
+
+
 }
