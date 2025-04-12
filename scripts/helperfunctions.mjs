a822b910bf4e0337eecbfb496c4b505342f29878
m-meno <mackenzie.meno@gmail.com>
2025-04-11 18:30:40 -0600

    helper function for event listener

--- scripts/helperfunctions.mjs
+++ scripts/helperfunctions.mjs
@@ -12,17 +12,39 @@
     }
 })();
 
-async function createDisplay(dataArray){
+let form = document.getElementById('breedForm');
+breedForm.addEventListener(`submit`, handleSubmit);    
+
+function createDisplay(dataArray){
     const main = document.getElementById(`main`);
     let list = document.createElement(`ul`);
     main.appendChild(list);
     console.log(dataArray);
     dataArray.forEach((el)=>{
         let listItem = document.createElement(`li`);
-        listItem.innerHTML = `Breed name: ${el.name}`
+        listItem.innerHTML = `Breed name: ${el.name}`;
         list.appendChild(listItem);
-    })
-
-
-
+        //console.log(el.name, el.id);
+    });
 }
+
+
+async function handleSubmit(e){
+    e.preventDefault();
+    console.log(`Testing`);
+    let input = document.getElementById(`breedSearch`);
+    console.log(input.value);
+    try {
+       if (input.value.length > 4 || input.value.length == 0) {
+        alert(`Incorrect search term - please enter at least 4 letters.`);
+        throw Error(`Incorrect or Empty SearchTerm`);
+       }
+
+       let res = await axios.get(`https://api.thedogapi.com/v1/images/search?breed_ids=${input.value}`);
+
+       console.log(res.data);
+    } catch(err) {
+        console.error(`Error`);
+    }
+};
+
